0a8a0b44379b246447b49b00a6df7f17586c67ec
Jhoceliz Figueroa <jhocelizf@gmail.com>
2023-03-11 15:43:08 -0300

    arregle el nav

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-// NAV
-const nav = document.querySelector("#nav");
-const abrir = document.querySelector("#abrir");
-const cerrar = document.querySelector("#cerrar");
-
-abrir.addEventListener("click", () => {
-    nav.classList.add("visible");
-})
-
-cerrar.addEventListener("click", () => {
-    nav.classList.remove("visible");
-})
 
 // PRODUCTOS
 // class
